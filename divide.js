5460d81cd1771f7e9681f44b7dc04fb69d48df74
OurSraushan <sraushan890@gmail.com>
2022-03-02 19:49:20 +0530

    all file uploaded

---
+++ divide.js
@@ -0,0 +1,12 @@
+
+let divide = () =>{
+
+    let a =20;
+    let b = 10;
+
+    let res = (20/10);
+    
+    return res;
+}
+
+module.exports = divide
---
+++ substract.js
@@ -0,0 +1,16 @@
+let substract =() =>{
+    let a =30;
+    let b =25;
+    let sub = 0;
+    sub = a-b;
+
+    return sub;
+}
+
+// let playVideos =()=>{
+
+//     console.log('Play Videos')
+// }
+
+
+module.exports = substract;
